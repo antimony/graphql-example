059addbb6eb7995ca7d1bbf1b6d57cdff4d9f721
Mikhail Surin <antimony@yandex.ru>
2023-01-14 00:27:00 +0300

    Update spqr to 0.12 and graphql-java to 20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 plugins {
     java
     idea
-    id("org.springframework.boot") version "2.4.5"
-    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
 }
 
 group = "su.mhl"
-version = "2021.05.14"
+version = "2023.01.12"
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 sourceSets {
     main {
@@ -24,11 +24,14 @@ sourceSets {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("io.leangen.graphql:graphql-spqr-spring-boot-starter:0.0.6")
+    implementation("io.leangen.graphql:graphql-spqr-spring-boot-starter:0.0.7")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+tasks.withType<Copy>() {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
